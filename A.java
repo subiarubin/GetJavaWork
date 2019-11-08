c22541c475205280655d8e3724a847c7bbefb9f4
subiarubin <57392744+subiarubin@users.noreply.github.com>
2019-11-08 18:02:56 +0530

    Add files via upload

---
+++ A.java
@@ -0,0 +1,10 @@
+package Io;
+import java.util.Scanner;
+public class A {
+	public static void main(String[] args) {
+	Scanner sc =new Scanner(System.in);	
+     int i= sc.nextInt();
+     System.out.println(i);
+	}
+
+}
