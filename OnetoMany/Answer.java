9be536611683964406e7afbbe55ad0959047cef5
subiarubin <57392744+subiarubin@users.noreply.github.com>
2020-01-22 16:30:20 +0530

    Add files via upload

---
+++ OnetoMany/Answer.java
@@ -0,0 +1,28 @@
+package OnetoMany;
+
+public class Answer {  
+private int id;  
+private String answername;  
+private String postedBy;  
+//getters and setters  
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public String getAnswername() {
+	return answername;
+}
+public void setAnswername(String answername) {
+	this.answername = answername;
+}
+public String getPostedBy() {
+	return postedBy;
+}
+public void setPostedBy(String postedBy) {
+	this.postedBy = postedBy;
+}
+
+  
+}  
