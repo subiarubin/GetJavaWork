50706fe9f2920e304e5a438d4d591b879646671a
subiarubin <57392744+subiarubin@users.noreply.github.com>
2020-01-27 09:08:45 +0530

    Add files via upload

---
+++ javai18/src/javatpointi18/InternatinalizationDemo.java
@@ -0,0 +1,18 @@
+package javatpointi18;
+import java.util.Locale;  
+import java.util.ResourceBundle;  
+public class InternatinalizationDemo
+{  
+ public static void main(String[] args) 
+ {  
+  
+  ResourceBundle bundle = ResourceBundle.getBundle("MessageBundle", Locale.US);  
+  System.out.println("Message in "+Locale.US +":"+bundle.getString("greeting"));  
+  
+  //changing the default locale to indonasian   
+  Locale.setDefault(new Locale("in", "ID"));  
+  bundle = ResourceBundle.getBundle("MessageBundle");  
+  System.out.println("Message in "+Locale.getDefault()+":"+bundle.getString("greeting"));  
+  
+ }  
+}  
